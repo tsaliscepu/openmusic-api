c569b5ae28fe7ab3040561758f1026c9970ce9ef
tsaliscepu <tsaliscepu@gmail.com>
2023-06-28 21:36:41 +0700

    Register Albums & Songs Plugin di Server Hapi

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+
+// Albums
+const albums = require('./api/albums');
+const AlbumsService = require('./services/inMemory/AlbumsService');
+
+// Songs
+const songs = require('./api/songs');
+const SongsService = require('./services/inMemory/SongsService');
  
  
 const init = async () => {
+  const albumsService = new AlbumsService();
+  const songsService = new SongsService();
+
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -13,7 +23,20 @@ const init = async () => {
     },
   });
 
- server.route(routes);
+ await server.register([
+  {
+    plugin: albums,
+    options: {
+      service: albumsService,
+    },
+  },
+  {
+    plugin: songs,
+    options: {
+      service: songsService,
+    },
+  }
+ ]);
  
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
