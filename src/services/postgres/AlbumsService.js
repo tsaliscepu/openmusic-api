3ed1575b963f840fc1e58a4ae910cb63fc3ae7f9
tsaliscepu <tsaliscepu@gmail.com>
2023-07-01 17:32:15 +0700

    Make editAlbum&SongById & deleteAlbum&SongById

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -55,4 +55,32 @@ class AlbumsService {
             songs: resultSong.rows,s
         };
     }
+
+    async editAlbumById(id, { name, year }) {
+        const query = {
+            text: 'UPDATE albums SET name = $1, year = $2 WHERE id = $3 RETURNING id',
+            values: [name, year, id],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Gagal memperbarui Album. Id tidak ditemukan');
+        }
+    }
+
+    async deleteAlbumById(id) {
+        const query = {
+            text: 'DELETE FROM albums WHERE id = $1 RETURNING id',
+            values: [id],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Album gagal dihapus. Id tidak ditemukan');
+        }
+    }
 }
+
+module.exports = AlbumsService;
--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -59,4 +59,32 @@ class SongsService {
 
         return result.rows.map(mapSongs)[0];
     }
+
+    async editSongById(id, { title, year, genre, performer, duration, albumId }) {
+        const query = {
+            text: 'UPDATE songs SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, album_id = $6 WHERE id = $7 RETURNING id',
+            values: [title, year, genre, performer, duration, albumId, id],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Gagal memperbarui. Id tidak ditemukan');
+        }
+    }
+
+    async deleteSongById(id) {
+        const query = {
+            text: 'DELETE FROM songs WHERE id = $1 RETURNING id',
+            values: [id],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Lagu gagal dihapus. Id tidak ditemukan');
+        }
+    }
 }
+
+module.exports = SongsService;
