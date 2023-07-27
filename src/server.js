7eaec47fb98b9018dab2b0c2201c59d3896ddaba
tsaliscepu <tsaliscepu@gmail.com>
2023-07-27 20:03:02 +0700

    Menggunakan Authentications Plugin pd Hapi Server

--- src/server.js
+++ src/server.js
@@ -17,10 +17,17 @@ const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
+// Authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const init = async () => {
   const albumsService = new AlbumsService()
   const songsService = new SongsService()
   const usersService = new UsersService()
+  const authenticationsService = new AuthenticationsService()
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -53,6 +60,15 @@ const init = async () => {
         service: usersService,
         validator: UsersValidator,
       }
+    },
+    {
+      plugin: authentications,
+      options: {
+        authenticationsService,
+        usersService,
+        tokenManager: TokenManager,
+        validator: AuthenticationsValidator,
+      },
     }
   ])
 
