c27eba2ce985ad627067bf3869a43c989f408919
tsaliscepu <tsaliscepu@gmail.com>
2023-07-27 16:56:47 +0700

    Membuat Fungsi getUserById

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -2,6 +2,7 @@ const { nanoid } = require("nanoid");
 const { Pool } = require("pg");
 const bcrypt = require('bcrypt');
 const InvariantError = require("../../exceptions/InvariantError");
+const NotFoundError = require("../../exceptions/NotFoundError");
 
 class UsersService {
     constructor() {
@@ -40,4 +41,21 @@ class UsersService {
             throw new InvariantError('Gagal menambahkan user. Username sudah digunakan.');
         }
     }
+
+    async getUserById(userId) {
+        const query = {
+            text: 'SELECT id, username, fullname FROM users WHERE id = $1',
+            values: [userId],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('User tidak ditemukan');
+        }
+
+        return result.rows[0];
+    }
 }
+
+module.exports = UsersService;
