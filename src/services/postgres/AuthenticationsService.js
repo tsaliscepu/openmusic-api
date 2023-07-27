6a4451dc222238ac6d68f437648f16b99a6963d7
tsaliscepu <tsaliscepu@gmail.com>
2023-07-27 19:17:57 +0700

    Membuat Fungsi addRefreshToken

--- src/services/postgres/AuthenticationsService.js
+++ src/services/postgres/AuthenticationsService.js
@@ -4,4 +4,13 @@ class AuthenticationsService {
     constructor() {
         this._pool = new Pool();
     }
+
+    async addRefreshToken(token) {
+        const query = {
+            text: 'INSERT INTO authentications VALUES($1)',
+            values: [token],
+        };
+
+        await this._pool.query(query);
+    }
 }
